9468f6b334fdef150b730b53f46e9787f1a8221b
Emile EKANE <ekaneekane3@gmail.com>
2021-11-04 12:49:47 +0100

    [ADD] Trees species and number of trees by kind jobs added

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,5 +1,8 @@
 package com.opstty;
 
+import com.opstty.job.District;
+import com.opstty.job.Species;
+import com.opstty.job.Treeskind;
 import com.opstty.job.WordCount;
 import org.apache.hadoop.util.ProgramDriver;
 
@@ -9,9 +12,10 @@ public class AppDriver {
         ProgramDriver programDriver = new ProgramDriver();
 
         try {
-            programDriver.addClass("wordcount", WordCount.class,
-                    "A map/reduce program that counts the words in the input files.");
-
+            programDriver.addClass("wordcount", WordCount.class,"A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("district", District.class ,"A map/reduce program that displays the list of distinct containing trees in this file.");
+            programDriver.addClass("species", Species.class ,"A map/reduce program that displays  the list of different species trees in this file.");
+            programDriver.addClass("treeskind", Treeskind.class,"A map/reduce program that calculates the number of trees of each kinds");
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
             throwable.printStackTrace();
